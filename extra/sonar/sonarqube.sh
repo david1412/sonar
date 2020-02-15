47f14d0e4553af10165b4860f19ec5978d78341a
Gopalam Moram <30881405+david1412@users.noreply.github.com>
2020-02-15 17:52:06 +0100

    Update sonarqube.sh

--- extra/sonar/sonarqube.sh
+++ extra/sonar/sonarqube.sh
@@ -1,1 +1,11 @@
-...
+#!/bin/bash
+
+set -x 
+    cd /opt 
+    curl -o sonarqube.zip -fSL https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-$SONAR_VERSION.zip 
+    curl -o sonarqube.zip.asc -fSL https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-$SONAR_VERSION.zip.asc 
+    gpg --batch --verify sonarqube.zip.asc sonarqube.zip 
+    unzip -q sonarqube.zip 
+    mv sonarqube-$SONAR_VERSION sonarqube 
+    chown -R sonarqube:sonarqube sonarqube 
+    rm sonarqube.zip* 
